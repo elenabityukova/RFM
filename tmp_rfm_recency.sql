969a7f251c211b8863847b9cc8047f21c9b8ed2a
elenabityukova <111985832+elenabityukova@users.noreply.github.com>
2022-12-19 20:52:34 +0700

    Update tmp_rfm_recency.sql

--- tmp_rfm_recency.sql
+++ tmp_rfm_recency.sql
@@ -1,27 +1,28 @@
 truncate analysis.tmp_rfm_recency;
 
 insert into analysis.tmp_rfm_recency 
-with o as (
-	select o.user_id, o.order_ts
-	from analysis.orders o, analysis.orderstatuses os 
-	where os."key" = 'Closed' and o.status = os.id 
-		and o.order_ts >= '01.01.2022'::timestamp	
+WITH order_closed AS(
+SELECT u.id,
+	   max(o.order_ts) AS orders_closed_last_date,
+       count(o.order_ts) AS orders_closed_cnt,
+	   sum(o.payment) AS orders_closed_sum
+FROM analysis.users u
+JOIN analysis.orders o ON o.user_id = u.id
+JOIN analysis.orderstatuses os ON o.status = os.id
+WHERE os."key" = 'Closed'
+  AND o.order_ts >= '01.01.2022'::timestamp
+GROUP BY u.id
 ),
-uo as (
-	select u.id, 
-		max(coalesce(o.order_ts,'01.01.2022'::timestamp))
-		 as "date_last_order"
-	from analysis.users u
-	left join o on o.user_id = u.id 
-	group by u.id
-	),
-recency as
-	(select 
-		date_last_order,
-		ntile(5) OVER( ORDER BY t.date_last_order nulls first) as "recency"
-	from (select date_last_order from uo group by date_last_order) t )
 
-SELECT uo.id as "user_id",   
-       recency.recency
-from uo
-join recency on recency.date_last_order = uo.date_last_order
+ds AS(
+SELECT u.id AS user_id,
+       coalesce(oc.orders_closed_last_date, '01.01.2022'::timestamp) AS orders_closed_last_date,
+       coalesce(oc.orders_closed_cnt, 0) AS orders_closed_cnt,
+       coalesce(oc.orders_closed_sum, 0) AS orders_closed_sum
+FROM analysis.users u
+LEFT JOIN order_closed oc ON oc.id = u.id
+)
+
+SELECT user_id,
+ntile(5) OVER( ORDER BY orders_closed_last_date) as "recency"
+FROM ds;
